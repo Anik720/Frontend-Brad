1ceebbfa415e7bff165a73860e3ef5ca9efbea2b
Anik7200 <anik.singularity@gmail.com>
2022-09-23 15:30:09 +0600

    doing

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 const CartScreen = ({}) => {
 const location=useLocation()
   const qty=location.search
-    console.log("hello",qty)
+  
   return (
     <div>
       Cart
